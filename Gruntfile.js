25930548adfaed930b06cff6a9c3d00ab8844e3b
Matthew Forr <matthew.forr@gmail.com>
2013-06-07 17:07:37 -0400

    Initial working beta

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,9 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      files: ['<%= pkg.axure_prototype %>*.html'],
-      tasks: ['copy', 'updateFiles'],
-      options: {
-        nospawn: true,
-        event: 'all'
-      }
+    axure_prototype: '<%= pkg.axure_prototype %>',
+    staticFilesRun: {
+      src: ['<%= pkg.axure_prototype %>*.html']
     },
     copy: {
       main: {
@@ -26,56 +22,26 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Empty object to hold the changed files
-  var changedFiles = [];
-
   // We never process these files
-  var excludes = [  '../../Documents/Axure/Prototypes/PublicKnowledge/chm_start.html',
-                    '../../Documents/Axure/Prototypes/PublicKnowledge/start.html',
-                    '../../Documents/Axure/Prototypes/PublicKnowledge/index.html'];
+  var excludes = [  grunt.config('axure_prototype') + 'chm_start.html',
+                    grunt.config('axure_prototype') + 'start.html',
+                    grunt.config('axure_prototype') + 'index.html'];
 
-  // Our task for updating the files
-  grunt.registerTask('updateFiles', function () {
+  grunt.registerMultiTask('staticFilesRun', function (arg) {
+    files = _.without.apply(_, [this.filesSrc].concat(excludes));
 
-    // First we exclude the usual suspects
-    // This is a fancy way of calling _.without with an Arrray for the excludes
-    changedFiles = _.without.apply(_, [changedFiles].concat(excludes));
-
-    // Then ensure we don't have any dupes
-    changedFiles = _.uniq(changedFiles);
-
-    // And next iterate over the files to be changed
-    _.each(changedFiles, function (file) {
-      var toWrite = '';
-
-      // We remove it now because this can get called multiple times and writeFile is async
-      // this prevents double updates.
-      changedFiles = _.without(changedFiles, file);
-
-      // Read the file in and look for the right spot to insert the css file.
-      fs.readFileSync(file).toString().split('\n').forEach(function (line) {
-        if (line.indexOf("</head>") !== -1) {
-          toWrite += '<link href="resources/css/fonts.css" type="text/css" rel="stylesheet"> \n';
-          toWrite += line + '\n';
-        } else {
-          toWrite += line + '\n';
-        }
-      });
-
-      // Replace the file with the inserted CSS Line
-      fs.writeFile(file, toWrite, function (err) {
-        if (err) {
-          throw err;
-        }
-
-        // Notify the user of success
-        grunt.log.write('Axure file successfully updated ' + file + ' ').ok();
-      });
+    _.each(files, function (file) {
+      var currentFile = grunt.file.read(file);
+      if (currentFile.indexOf("fonts.css") === -1) {
+        currentFile = currentFile.replace('</head>',
+          '    <link href="resources/css/fonts.css" type="text/css" rel="stylesheet">\n</head>');
+        grunt.file.write(file, currentFile);
+        grunt.log.ok(file + ' updated');
+      } else {
+        grunt.log.error(file + ' already has fonts.css added');
+      }
     });
   });
 
-  grunt.event.on('watch', function (action, filepath) {
-    // When a file changes it, add it to the changedFiles queue.
-    changedFiles.push(filepath);
-  });
+  grunt.registerTask('default', ['staticFilesRun', 'copy']);
 };
